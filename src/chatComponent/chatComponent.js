5ec8c32ffb4cacd378f8d28872b7fb4a24600249
Mohamed Aziz Bennour <MBennour@se.linedata.com>
2023-02-21 14:29:13 +0100

    dialogflow commit

---
+++ src/chatComponent/chatComponent.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+const chatComponent = () => {
+ 
+
+  return (
+    <div>
+    
+    <iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/229ace50-e191-4d98-9ac7-ab37e13ee00d">
+    </iframe>    
+    </div>
+  );
+};
+
+export default chatComponent;
